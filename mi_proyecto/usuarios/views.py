9d2223f65c6954e79143546e4f3bce76a6d61912
Haans Lopez <jesuslopez@ciencias.unam.mx>
2025-02-24 20:44:59 -0600

    <FEAT>: addign login and logout with dummy dashboard

---
+++ mi_proyecto/usuarios/views.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, "Usuario o contraseña incorrectos")
+    
+    return render(request, 'usuarios/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def dashboard(request):
+    return render(request, 'usuarios/dashboard.html')
